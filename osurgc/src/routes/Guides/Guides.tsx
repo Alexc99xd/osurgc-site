414abde938582bba992fcd89491aeb754d11755f
Alex Chan <chan.730@osu.edu>
2023-06-12 20:35:37 -0400

    guide lol

--- osurgc/src/routes/Guides/Guides.tsx
+++ osurgc/src/routes/Guides/Guides.tsx
@@ -3,6 +3,16 @@ function Guides() {
     return (
       <>
         <h1>Arcade guides</h1>
+        <a href='https://docs.google.com/document/d/12Ou6Jc-fT_inzauCTq_YWUk-Xt0aATcrqLVAiYMBQ7A/edit'>Round 1 guide</a>
+
+        <h2>Game guides</h2>
+        <a>SDVX guide</a>
+        <a>DDR guide</a>
+        <a>PUI guide</a>
+        <a>Wacca guide</a>
+        <a>chunithm guide</a>
+        <a>drummania guide</a>
+        <a>iidx guide</a>
       </>
     )
   }
